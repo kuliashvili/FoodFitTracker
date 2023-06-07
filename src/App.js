a2c8a30b77800504b16ecc5adfc1c6eb5389d41a
yulo01 <giorgi.kuliashvili.1@btu.edu.ge>
2023-06-07 15:28:02 +0400

    finishing algorithms

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const APP_ID = '0b6a6537';
+  const API_KEY = '3cff7ff6aabeefac5115b864faf35bf7';
+  const [inputValue, setInputValue] = useState('')
+  const [inputQuantityValue, setInputQuantityValue] = useState()
+  // let food = document.getElementById('food-input').value
+  const ingr = 'egg'; // Example ingredient
+  const [calories, setCalories] = useState('')
+  const [weight, setWeight] = useState('')  
+
+  const updateInputValue = (e) => {
+    let foodName = e.target.value
+    setInputValue(foodName)
+    console.log(foodName)
+    
+  }
+  const updateInputQuantityValue = (e) => {
+    let foodQuantity = e.target.value
+    setInputQuantityValue(foodQuantity)
+    console.log(foodQuantity)
+    
+  }
+console.log(inputValue)
+  useEffect(() => {
+    axios
+      .get(`https://api.edamam.com/api/nutrition-data?app_id=${APP_ID}&app_key=${API_KEY}&ingr=${inputQuantityValue == undefined ? "1" + inputValue : inputQuantityValue + inputValue}`)
+      .then(res => {
+        // console.log(res.data.calories);
+        setCalories(res.data.calories)
+        // console.log(res.data.totalWeight);
+        setWeight(res.data.totalWeight)
+        
+      })
+      .catch(err => console.log(err));
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>input food:</h2>
+      <input onChange={updateInputValue} id='food-input' className='food-input' type="text"  />
+      <h2>input food quantity:</h2>
+      <input onChange={updateInputQuantityValue} className='foot-input' type="text" />
+      <h1 className='food'>{inputValue == "" ? "write food in input :)" : inputValue}</h1>
+      <h1>{"calories of the food = " + calories + " cal"}</h1>
+      <h1>{"weight of the food = " + weight + " gr"}</h1>
     </div>
   );
 }
